bf8f883a20eaf84c70ad976549151781f6789a76
JingqiSunn <142304970+JingqiSunn@users.noreply.github.com>
2024-04-22 08:30:04 +0800

    Introduce projector

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,9 +1,10 @@
 import GameElement.ControllingCenter;
-
+import GameViual.Projector;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Projector.launch(Projector.class, args);
         Scanner fetch = new Scanner(System.in);
         ControllingCenter controllingCenter = new ControllingCenter();
         controllingCenter.GetTheSetUpInformationOfTheBoard(fetch);
